3ae1d495a1fff365dddb1f25aa7077e1f9a4393f
tejas <tejas.rkd@gmail.com>
2023-07-20 11:34:45 +0530

    RSA AES Chats

---
+++ data.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/rsa"
+	"net"
+)
+
+const (
+	Certificate      int = 0
+	SharedSecret     int = 1
+	EncryptedMessage int = 2
+	Error            int = 3
+)
+
+type Connection struct {
+	id     int64
+	secret []byte
+	socket net.Conn
+	data   chan []byte
+}
+
+type Message struct {
+	SenderId     int64  `json:"senderId"`
+	RecieverId   int64  `json:"recieverId"`
+	EncryptedMsg []byte `json:"encryptedMsg"`
+	OpCode       int    `json:"opCode"`
+}
+
+type ConnectionManager struct {
+	publicKey   rsa.PublicKey
+	privateKey  rsa.PrivateKey
+	lPort       int64
+	connections map[int64]*Connection
+	register    chan *Connection
+	unregister  chan *Connection
+}
